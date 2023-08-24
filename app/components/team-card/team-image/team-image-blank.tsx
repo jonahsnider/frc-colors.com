2a5df40041e1fa0c554d75996a7f135819c4e1cd
Jonah Snider <jonah@jonahsnider.com>
2023-08-24 08:29:10 -0700

    feat: make the blank team avatar image use a gradient

--- app/components/team-card/team-image/team-image-blank.tsx
+++ app/components/team-card/team-image/team-image-blank.tsx
@@ -1,3 +1,5 @@
+import TeamImageGradient from './team-image-gradient';
+
 export default function TeamImageBlank() {
-	return <div className='bg-gray-500 rounded transition-all w-48 h-48' />;
+	return <TeamImageGradient colors={{ primaryHex: '#6b7280', secondaryHex: '#1f2937' }} />;
 }
