b22aedccc79762e330761947913c934e29d42d49
Jonah Snider <jonah@jonahsnider.com>
2023-08-24 11:37:10 -0700

    feat: add credits to footer & stick to bottom

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,17 @@
 export default function Footer() {
 	return (
-		<footer className='text-center py-5 text-neutral-600'>
+		<footer className='text-center py-5 text-neutral-600 absolute w-full bottom-0'>
 			<p>
 				FRC Colors is licensed under{' '}
 				<a className='underline' href='https://www.apache.org/licenses/LICENSE-2.0'>
 					Apache 2.0
+				</a>
+			</p>
+
+			<p>
+				Created by{' '}
+				<a className='underline' href='https://jonahsnider.com'>
+					Jonah Snider
 				</a>
 			</p>
 		</footer>
