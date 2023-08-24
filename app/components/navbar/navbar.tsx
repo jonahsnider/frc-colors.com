e698f393d49a490aa1650fe33c10a77033d2486b
Jonah Snider <jonah@jonahsnider.com>
2023-08-24 12:48:14 -0700

    fix: prevent navbar from getting too wide

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -19,23 +19,25 @@ const items: NavbarItem[] = [
 
 export default function Navbar() {
 	return (
-		<nav className='w-full flex justify-between bg-neutral-800 text-zinc-100 px-4 py-2'>
-			<NavbarLogo />
+		<nav className='w-full bg-neutral-800 text-zinc-100 px-4 py-2 flex justify-center'>
+			<div className='self-center flex justify-between w-full max-w-4xl'>
+				<NavbarLogo />
 
-			<ul className='flex flex-row space-x-2'>
-				{items.map((item, index) => {
-					return (
-						<li
-							key={item.content}
-							className='px-1.5 py-0.5 self-center rounded transition-colors hover:bg-neutral-700 active:bg-neutral-600'
-						>
-							<Link href={item.href} className='text-lg'>
-								{item.content}
-							</Link>
-						</li>
-					);
-				})}
-			</ul>
+				<ul className='flex flex-row space-x-2'>
+					{items.map((item, index) => {
+						return (
+							<li
+								key={item.content}
+								className='px-1.5 py-0.5 self-center rounded transition-colors hover:bg-neutral-700 active:bg-neutral-600'
+							>
+								<Link href={item.href} className='text-lg'>
+									{item.content}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</nav>
 	);
 }
