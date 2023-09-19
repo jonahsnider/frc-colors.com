33bf1e6a99e2baadd5d8a63fae82c82812d59153
Jonah Snider <jonah@jonahsnider.com>
2023-09-19 00:26:17 -0700

    feat: add Prisma integration to Sentry performance monitoring

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,10 @@
 import * as Sentry from '@sentry/nextjs';
+import { prisma } from './app/api/_lib/prisma';
 
 Sentry.init({
 	dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
 	tracesSampleRate: 1.0,
+
+	integrations: [new Sentry.Integrations.Prisma({ client: prisma })],
 });
