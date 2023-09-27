51079ab3c3d500d8b00080787e617e85e9393158
Jonah Snider <jonah@jonahsnider.com>
2023-09-27 16:50:14 -0700

    refactor: improve types in migration script

--- migrate.ts
+++ migrate.ts
@@ -1,10 +1,11 @@
 import { sql } from '@vercel/postgres';
+import { MigrationConfig } from 'drizzle-orm/migrator';
 import { drizzle } from 'drizzle-orm/vercel-postgres';
 import { migrate } from 'drizzle-orm/vercel-postgres/migrator';
 
 const migrationOptions = {
 	migrationsFolder: './drizzle',
-};
+} satisfies MigrationConfig;
 
 const db = drizzle(sql);
 
