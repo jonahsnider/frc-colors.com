e59a13ed92c2d0b6283fde9a57ea903a114dcf62
Jonah Snider <jonah@jonahsnider.com>
2023-11-13 20:09:39 -0800

    fix: ignore 10000001 team number from Statbotics

--- app/api/_lib/teams/dtos/team-number.dto.ts
+++ app/api/_lib/teams/dtos/team-number.dto.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 
 export const TeamNumberSchema = z
-	.string()
-	.regex(/^\d+$/)
-	.or(z.number())
-	.transform(Number)
+	.union([
+		z.string().regex(/^\d+$/).transform(Number),
+		z.number(),
+		// For some reason, Statbotics has started including a 10000001 team at the end of their requests
+		// It can just be ignored on our end
+		z.literal(10000001),
+	])
 	.pipe(z.number().positive().int().max(50_000));
 export type TeamNumberSchema = z.infer<typeof TeamNumberSchema>;
