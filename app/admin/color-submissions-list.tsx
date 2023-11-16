5bc79940f9730951b336952003fa2613c9414840
Jonah Snider <jonah@jonahsnider.com>
2023-11-15 16:33:38 -0800

    feat: add pending submissions and requests count to admin page

--- app/admin/color-submissions-list.tsx
+++ app/admin/color-submissions-list.tsx
@@ -4,6 +4,8 @@ import ColorSubmissionsTable from '../components/admin/color-submissions/table';
 import H2 from '../components/headings/h2';
 import { useApiKey } from '../hooks/use-api-key';
 import { fetcherWithApiKey } from '../swr';
+import { Schema } from '../api/_lib/db/index';
+import { count } from '@jonahsnider/util';
 
 export default function ColorSubmissionsList() {
 	const [apiKey] = useApiKey();
@@ -11,11 +13,35 @@ export default function ColorSubmissionsList() {
 		fetcher: apiKey ? fetcherWithApiKey : undefined,
 	});
 
+	const pendingSubmissions = {
+		total: count(
+			data?.colorSubmissions ?? [],
+			(submission) => submission.status === Schema.VerificationRequestStatus.Pending,
+		),
+		unique: count(
+			new Set(
+				(data?.colorSubmissions ?? [])
+					.filter((submission) => submission.status === Schema.VerificationRequestStatus.Pending)
+					.map((submission) => submission.teamNumber),
+			),
+		),
+	};
+
 	return (
 		<div className='flex flex-col items-center gap-y-4'>
 			<H2>Color submissions</H2>
 
-			{data && <ColorSubmissionsTable colorSubmissions={data.colorSubmissions} />}
+			{data && (
+				<>
+					{Boolean(pendingSubmissions.total) && (
+						<p>
+							{pendingSubmissions.total.toLocaleString()} submissions pending (
+							{pendingSubmissions.unique.toLocaleString()} unique teams)
+						</p>
+					)}
+					<ColorSubmissionsTable colorSubmissions={data.colorSubmissions} />
+				</>
+			)}
 
 			{isLoading && <div>Loading...</div>}
 
--- app/admin/verification-requests-list.tsx
+++ app/admin/verification-requests-list.tsx
@@ -4,6 +4,8 @@ import VerificationRequestsTable from '../components/admin/verification-requests
 import H2 from '../components/headings/h2';
 import { useApiKey } from '../hooks/use-api-key';
 import { fetcherWithApiKey } from '../swr';
+import { count } from '@jonahsnider/util';
+import { Schema } from '../api/_lib/db/index';
 
 export default function VerificationRequestsList() {
 	const [apiKey] = useApiKey();
@@ -14,11 +16,35 @@ export default function VerificationRequestsList() {
 		},
 	);
 
+	const pendingRequests = {
+		total: count(
+			data?.verificationRequests ?? [],
+			(request) => request.status === Schema.VerificationRequestStatus.Pending,
+		),
+		unique: count(
+			new Set(
+				(data?.verificationRequests ?? [])
+					.filter((request) => request.status === Schema.VerificationRequestStatus.Pending)
+					.map((request) => request.team),
+			),
+		),
+	};
+
 	return (
 		<div className='flex flex-col items-center gap-y-4'>
 			<H2>Verification requests</H2>
 
-			{data && <VerificationRequestsTable requests={data.verificationRequests} />}
+			{data && (
+				<>
+					{Boolean(pendingRequests.total) && (
+						<p>
+							{pendingRequests.total.toLocaleString()} requests pending ({pendingRequests.unique.toLocaleString()}{' '}
+							unique teams)
+						</p>
+					)}
+					<VerificationRequestsTable requests={data.verificationRequests} />
+				</>
+			)}
 
 			{isLoading && <div>Loading...</div>}
 
