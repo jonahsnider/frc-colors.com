88e1273f5b804d444a7093ba67587ff614a4a41c
Jonah Snider <jonah@jonahsnider.com>
2023-11-15 16:12:44 -0800

    feat: mark teams as invalid on home page if they don't exist

--- app/components/search-teams.tsx
+++ app/components/search-teams.tsx
@@ -1,21 +1,24 @@
 'use client';
 
 import clsx from 'clsx';
-import { useSearchParams } from 'next/navigation';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 import { TeamNumberSchema } from '../api/_lib/teams/dtos/team-number.dto';
 import { TeamNumberContext } from '../contexts/team-number-context';
 
-export default function SearchTeams() {
+type Props = {
+	invalidTeam: boolean;
+};
+
+export default function SearchTeams({ invalidTeam }: Props) {
 	const searchParams = useSearchParams();
 	const urlTeam = searchParams.get('team') ?? undefined;
 	const router = useRouter();
 
 	const [teamNumberRaw, setTeamNumberRaw] = useState(urlTeam ?? '');
 	const setTeamNumber = useDebouncedCallback(useContext(TeamNumberContext).setTeamNumber, 100, { maxWait: 1000 });
-	const valid = teamNumberRaw === '' || TeamNumberSchema.safeParse(teamNumberRaw).success;
+	const valid = teamNumberRaw === '' || (TeamNumberSchema.safeParse(teamNumberRaw).success && !invalidTeam);
 
 	// biome-ignore lint/correctness/useExhaustiveDependencies: This is only supposed to run once on render
 	useEffect(() => {
@@ -26,15 +29,23 @@ export default function SearchTeams() {
 	}, []);
 
 	useEffect(() => {
-		router.push(`/?team=${teamNumberRaw}`);
+		if (teamNumberRaw === '') {
+			router.push('/');
+		} else {
+			router.push(`/?team=${teamNumberRaw}`);
+		}
 	}, [teamNumberRaw, router]);
 
 	return (
 		<div className='flex flex-col gap-y-4'>
 			<input
-				className={clsx('transition-all h-14 rounded p-4 outline-none bg-neutral-800 shadow shadow-neutral-950', {
-					'border-4 border-red-400': !valid,
-				})}
+				className={clsx(
+					'transition-all h-14 rounded p-4 outline-none bg-neutral-800 shadow shadow-neutral-950 border-4',
+					{
+						'border-red-400': !valid,
+						'border-transparent': valid,
+					},
+				)}
 				// biome-ignore lint/a11y/noAutofocus: Autofocus is essential to have here
 				autoFocus={true}
 				placeholder='Enter a team number'
