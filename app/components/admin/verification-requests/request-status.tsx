3afbe0898599ad142ed39c183af1a778c745aa73
Jonah Snider <jonah@jonahsnider.com>
2023-11-15 16:17:41 -0800

    feat: add tooltip for updated at date in verification request

--- app/components/admin/verification-requests/request-status.tsx
+++ app/components/admin/verification-requests/request-status.tsx
@@ -1,19 +1,22 @@
 import { Schema } from '@/app/api/_lib/db/index';
 import { VerificationRequestSchema } from '@/app/api/_lib/teams/verification-requests/dtos/verification-request.dto';
 import { CheckIcon, ExclamationCircleIcon } from '@heroicons/react/20/solid';
+import { formatDistanceToNow } from 'date-fns';
 
 type Props = {
 	request: VerificationRequestSchema;
 };
 
 export default function RequestStatus({ request }: Props) {
+	const updatedAt = request.updatedAt ? `Updated ${formatDistanceToNow(new Date(request.updatedAt))} ago` : undefined;
+
 	if (request.status === Schema.VerificationRequestStatus.Finished) {
-		return <CheckIcon className='h-6 text-gray-400' />;
+		return <CheckIcon className='h-6 text-gray-400' title={updatedAt} />;
 	}
 
 	if (request.status === Schema.VerificationRequestStatus.Pending) {
 		return <ExclamationCircleIcon className='h-6 text-yellow-400' />;
 	}
 
-	return <CheckIcon className='h-6 text-red-400' />;
+	return <CheckIcon className='h-6 text-red-400' title={updatedAt} />;
 }
