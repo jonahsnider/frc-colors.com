085834132752e34590ef63288a49e78365d1b62f
Jonah Snider <jonah@jonahsnider.com>
2023-11-16 00:02:01 -0800

    fix: remove special case for team number validation

--- app/api/_lib/teams/dtos/team-number.dto.ts
+++ app/api/_lib/teams/dtos/team-number.dto.ts
@@ -1,10 +1,6 @@
 import { z } from 'zod';
 
-export const TeamNumberSchema = z.union([z.string().regex(/^\d+$/).transform(Number), z.number()]).pipe(
-	z.number().positive().int().max(50_000).or(
-		// For some reason, Statbotics has started including a 10000001 team at the end of their requests
-		// It can just be ignored on our end
-		z.literal(10000001),
-	),
-);
+export const TeamNumberSchema = z
+	.union([z.string().regex(/^\d+$/).transform(Number), z.number()])
+	.pipe(z.number().positive().int().max(50_000));
 export type TeamNumberSchema = z.infer<typeof TeamNumberSchema>;
