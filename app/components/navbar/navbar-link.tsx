d3525126da75524c0b24febeb869302299c02659
Jonah Snider <jonah@jonahsnider.com>
2023-11-15 16:02:35 -0800

    fix: make entire navbar link component clickable

--- app/components/navbar/navbar-link.tsx
+++ app/components/navbar/navbar-link.tsx
@@ -3,8 +3,11 @@ import { NavbarItem } from './types';
 
 export default function NavbarLink({ item }: { item: NavbarItem }) {
 	return (
-		<li className='px-1.5 py-0.5 self-center rounded transition-colors hover:bg-neutral-700 active:bg-neutral-600'>
-			<Link href={item.href} className='text-lg'>
+		<li className='flex items-center justify-center'>
+			<Link
+				href={item.href}
+				className='text-lg px-1.5 py-0.5 self-center rounded transition-colors hover:bg-neutral-700 active:bg-neutral-600'
+			>
 				{item.content}
 			</Link>
 		</li>
