9e2e83f21b061a3fa67795408cbd8378d0d9a4a2
Jonah Snider <jonah@jonahsnider.com>
2023-12-23 14:20:07 -0800

    feat: increase CACHE_TTL_TEAM_AVATAR and CACHE_TTL_VERIFIED_COLORS from 2 weeks to 1 month

--- app/api/_lib/config/ttls-config.ts
+++ app/api/_lib/config/ttls-config.ts
@@ -1,7 +1,7 @@
 import convert from 'convert';
 
 /** Duration to cache response for team media for year. */
-export const CACHE_TTL_TBA_TEAM_MEDIA = convert(1, 'week');
+export const CACHE_TTL_TBA_TEAM_MEDIA = convert(2, 'week');
 
 /** Duration to cache response for team. */
 export const CACHE_TTL_TBA_TEAM = convert(2, 'week');
@@ -10,7 +10,7 @@ export const CACHE_TTL_TBA_TEAM = convert(2, 'week');
 export const CACHE_TTL_TBA_EVENT_TEAMS = convert(1, 'week');
 
 /** Lifetime of DB rows for caching team avatars. */
-export const CACHE_TTL_TEAM_AVATAR = convert(14, 'days');
+export const CACHE_TTL_TEAM_AVATAR = convert(1, 'month');
 
 /** Lifetime of Redis key for caching generated colors. */
 export const CACHE_TTL_GENERATED_COLORS = CACHE_TTL_TEAM_AVATAR;
