58fce4267b0b20dab5be93f7bcab3a92cb5e1ad7
Jonah Snider <jonah@jonahsnider.com>
2024-01-28 04:20:03 -0800

    fix(web): fix /api redirect when query parameters are present

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -3,7 +3,10 @@ import getBaseApiUrl from './shared';
 
 export function middleware(request: NextRequest) {
 	if (request.nextUrl.pathname.startsWith('/api')) {
-		const targetUrl = new URL(request.nextUrl.pathname.slice('/api'.length), getBaseApiUrl());
+		const targetUrl = request.nextUrl;
+
+		targetUrl.pathname = targetUrl.pathname.slice('/api'.length);
+		targetUrl.host = new URL(getBaseApiUrl()).host;
 
 		return NextResponse.redirect(targetUrl, {
 			headers: {
