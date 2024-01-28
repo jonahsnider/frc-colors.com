9c17b9e122e5a3df7165d6ed280e3fb45aabc2e7
Jonah Snider <jonah@jonahsnider.com>
2024-01-28 01:15:40 -0800

    refactor: remove debug script

--- apps/api/run.sh
+++
@@ -1,7 +0,0 @@
-POSTGRES_URL="postgres://default:u6a4SZYAnLgv@ep-damp-flower-96031569-pooler.us-east-1.postgres.vercel-storage.com/verceldb"
-
-echo $POSTGRES_URL
-
-bun run build
-
-./dist/frc-colors
