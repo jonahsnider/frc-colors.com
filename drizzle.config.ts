005b1c64d694a36d2eb77bcfa95b04088466d4ed
Jonah Snider <jonah@jonahsnider.com>
2024-01-28 01:20:54 -0800

    refactor: remove dotenv dependency from migration script

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,5 @@
 import assert from 'assert';
-import { config } from 'dotenv';
 import type { Config } from 'drizzle-kit';
-
-config();
 
 const connectionString = process.env.POSTGRES_URL_NON_POOLING ?? process.env.POSTGRES_URL;
 assert(connectionString, 'POSTGRES_URL_NON_POOLING or POSTGRES_URL environment variable must be defined');
