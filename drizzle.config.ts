49edb5bc76b8c2843fbee6a33b0772a7ca73c936
Jonah Snider <jonah@jonahsnider.com>
2024-01-30 00:04:54 -0800

    fix(api): fix Drizzle config having old DB connection env var

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,17 @@
-import assert from 'assert';
 import type { Config } from 'drizzle-kit';
+import { cleanEnv, str } from 'envalid';
 
-const connectionString = process.env.POSTGRES_URL_NON_POOLING ?? process.env.POSTGRES_URL;
-assert(connectionString, 'POSTGRES_URL_NON_POOLING or POSTGRES_URL environment variable must be defined');
+const env = cleanEnv(process.env, {
+	// biome-ignore lint/style/useNamingConvention: This is an environment variable
+	DATABASE_URL: str({ desc: 'PostgreSQL URL' }),
+});
 
 // biome-ignore lint/style/noDefaultExport: This must be a default export
 export default {
 	schema: 'apps/api/src/db/schema.ts',
 	driver: 'pg',
 	dbCredentials: {
-		connectionString,
+		connectionString: env.DATABASE_URL,
 	},
 	tablesFilter: ['!_prisma_migrations'],
 	out: 'drizzle',
