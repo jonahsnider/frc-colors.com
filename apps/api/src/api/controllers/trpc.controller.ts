8ebd1cb42f3f788afd8d37ca76ca338ff33807d8
Jonah Snider <jonah@jonahsnider.com>
2024-01-29 18:15:33 -0800

    fix(api): stop leaking server errors to clients via tRPC

--- apps/api/src/api/controllers/trpc.controller.ts
+++ apps/api/src/api/controllers/trpc.controller.ts
@@ -6,6 +6,15 @@ import { RegisterController } from '../interfaces/controller.interface';
 const handler = createHTTPHandler({
 	router: appRouter,
 	createContext,
+	onError: (options) => {
+		if (options.error.code === 'INTERNAL_SERVER_ERROR') {
+			// Actual server error, should throw
+			throw options.error;
+		}
+
+		// Use default TRPC error response for client errors
+		return;
+	},
 });
 
 export const trpcController: RegisterController = (app) => app.use('/trpc', handler).use('/trpc/*', handler);
