b1e299677e14fc0595cdc070235cddfa5a8d1683
Jonah Snider <jonah@jonahsnider.com>
2024-02-03 02:20:14 -0800

    feat(api): improve health check

--- apps/api/src/api/controllers/health.controller.ts
+++ apps/api/src/api/controllers/health.controller.ts
@@ -1,7 +1,11 @@
+import { storedColors } from '../../colors/stored/stored-colors.service';
 import { RegisterController } from '../interfaces/controller.interface';
 
 export const healthController: RegisterController = (app) =>
 	app.get('/health', async (_req, res) => {
+		// Check that querying DB works
+		await storedColors.getTeamColors(581);
+
 		res.json({
 			status: 'ok',
 		});
