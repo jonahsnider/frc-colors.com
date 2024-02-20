38feff2b852f4bd67cf66b60c24631ccf184cedc
Jonah Snider <jonah@jonahsnider.com>
2024-02-20 07:20:36 -0800

    fix(api): fix health check controller not having server timing enabled

--- apps/api/src/api/controllers/health.controller.ts
+++ apps/api/src/api/controllers/health.controller.ts
@@ -1,7 +1,8 @@
 import { Hono } from 'hono';
+import { timing } from 'hono/timing';
 import { colorsService } from '../../colors/colors.service';
 
-export const healthController = new Hono().get('/', async (context) => {
+export const healthController = new Hono().use(timing()).get('/', async (context) => {
 	// Check that querying DB works
 	await colorsService.stored.getTeamColors(581);
 
