61fe69181170e05867dd64e013a4097cd890a353
Jonah Snider <jonah@jonahsnider.com>
2024-02-29 20:33:45 -0800

    refactor: remove unused events tRPC router

--- apps/api/src/trpc/app.router.ts
+++ apps/api/src/trpc/app.router.ts
@@ -1,12 +1,10 @@
 import { analyticsRouter } from '../analytics/analytics.router';
 import { colorSubmissionsRouter } from '../color-submissions/color-submissions.router';
-import { eventsRouter } from '../events/events.router';
 import { teamsRouter } from '../teams/teams.router';
 import { verificationRequestsRouter } from '../verification-requests/verification-requests.router';
 import { router } from './trpc';
 
 export const appRouter = router({
-	events: eventsRouter,
 	teams: teamsRouter,
 	verificationRequests: verificationRequestsRouter,
 	colorSubmissions: colorSubmissionsRouter,
