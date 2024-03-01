b31c092cca22d3e425fcaf05ca0cb2dca8dc749d
Jonah Snider <jonah@jonahsnider.com>
2024-02-29 20:34:21 -0800

    feat(api): add analytics to tRPC routers

--- apps/api/src/teams/teams.router.ts
+++ apps/api/src/teams/teams.router.ts
@@ -1,4 +1,5 @@
 import { z } from 'zod';
+import { analyticsService } from '../analytics/analytics.service';
 import { colorsService } from '../colors/colors.service';
 import { ManyTeamColors, TeamColors } from '../colors/dtos/colors.dto';
 import { Schema } from '../db/index';
@@ -22,11 +23,29 @@ export const teamsRouter = router({
 		get: publicProcedure
 			.input(TeamNumber)
 			.output(z.object({ colors: TeamColors.optional() }))
-			.query(async ({ input }) => ({ colors: await colorsService.stored.getTeamColors(input) })),
+			.query(async ({ input, ctx }) => {
+				analyticsService.client.capture({
+					distinctId: ctx.requestIp,
+					event: 'get_team_colors',
+					properties: {
+						team: input,
+					},
+				});
+				return { colors: await colorsService.stored.getTeamColors(input) };
+			}),
 		getMany: publicProcedure
 			.input(TeamNumber.array())
 			.output(ManyTeamColors)
-			.query(({ input }) => colorsService.stored.getTeamColors(input)),
+			.query(async ({ input, ctx }) => {
+				analyticsService.client.capture({
+					distinctId: ctx.requestIp,
+					event: 'get_many_team_colors',
+					properties: {
+						teams: input,
+					},
+				});
+				return colorsService.stored.getTeamColors(input);
+			}),
 
 		set: adminProcedure.input(SetColorsInput).mutation(async ({ input }) => {
 			await colorsService.stored.setTeamColors(input.team, { ...input.colors, verified: true });
