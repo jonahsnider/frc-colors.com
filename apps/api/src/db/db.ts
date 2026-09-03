9e047b1abab190bc0b001b18ac1534320930bef0
Jonah Snider <jonah@jonahsnider.com>
2026-09-02 22:49:50 -0700

    refactor(api): replace pg with postgres
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- apps/api/src/db/db.ts
+++ apps/api/src/db/db.ts
@@ -1,11 +1,10 @@
 import { defineRelations } from 'drizzle-orm';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
 import { configService } from '../config/config.service.ts';
 import { Schema } from './index.ts';
 
 const relations = defineRelations(Schema);
 
-const client = new Client({ connectionString: configService.databaseUrl });
-await client.connect();
+const client = postgres(configService.databaseUrl);
 export const db = drizzle({ client, relations });
